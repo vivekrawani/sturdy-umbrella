163858af345f3457278afd9dcf03d9ca75553d54
Vivek <kumarrawanivivek@gmail.com>
2024-02-03 00:22:01 +0530

    persist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-
+"use client";
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 
 
-
-export default async function Home() {
-
+export default function Home() {
+  const router = useRouter();
+  const user = true;
+  if (!user) {
+    router.push('/login')
+  }
   return (
-
-    <div>
-      hi
+    <div className='p-2 flex'>
+      <h1 className='text-3xl'>Hey</h1>
     </div>
 
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,10 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+        "image-1" : 
+          "url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);",
+        },
+
     },
   },
   plugins: [],
