cf45ac87318820e6da635d58869f001b5ca336c0
Vivek <kumarrawanivivek@gmail.com>
2024-02-22 01:45:52 +0530

    s

--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -27,7 +27,7 @@ export default function OrderCard({ details }: { details: OrderDetails | null })
     }
     let deliverTimeF =  '';
     if(orderAcceptTime){
-        console.log(orderAcceptTime);
+        console.log("order accept time",orderAcceptTime);
 console.log("emty")
         // deliverTimeF = format(deliverTime, "PPp");
     }
