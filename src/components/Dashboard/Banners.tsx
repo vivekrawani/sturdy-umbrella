e7f27d91f0929dfc0fa95c4bfe1e8892e6333242
Vivek <kumarrawanivivek@gmail.com>
2024-04-10 16:12:37 +0530

    bug fix

--- src/components/Dashboard/Banners.tsx
+++ src/components/Dashboard/Banners.tsx
@@ -12,13 +12,13 @@ export default async function Banners() {
   return (
     <div className="bg-white rounded-md shadow-md shadow-slate-900 p-4">
       <h2 className="text-5xl font-bold">Banners</h2>
-      <div className="flex gap-4 flex-wrap p-3">
+      <div className="flex gap-4 flex-wrap overflow-y-scroll p-3">
         {
           banners.map((b: string, index: any) => (
-            <div className="flex flex-col items-center gap-2"
+            <div className="flex flex-col items-center gap-2 border-sky-200 border-2 px-2 pb-2"
               key={b}>
               <span className=" font-extrabold">{index + 1}</span>
-              <Image src={b} height={"200px"} width={"140px"} alt={b} />
+              <Image src={b} height={"200px"} width={"auto"} alt={b} />
               <DeleteButton index={index} />
             </div>)
           )}
@@ -29,6 +29,7 @@ export default async function Banners() {
         <AddBanner />
 
       </div>
+      <div>{JSON.stringify(new Date())}</div>
     </div>
   )
 }
