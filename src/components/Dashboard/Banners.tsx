d18c69e4d3f17a331ec37152920600aeb8a2d06b
Vivek <kumarrawanivivek@gmail.com>
2024-04-10 15:56:08 +0530

    fixed typo in banners

--- src/components/Dashboard/Banners.tsx
+++ src/components/Dashboard/Banners.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'auto'
+export const dynamic = 'force-dynamic'
 import { getBanner } from "@/db/firebase";
 import { Image } from "@chakra-ui/react";
 import AddBanner from "./AddBanner";
