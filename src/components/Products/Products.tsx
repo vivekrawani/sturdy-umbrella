a64d544ff360018ef537e00b9d24dc4edc9504d4
Vivek <kumarrawanivivek@gmail.com>
2024-06-06 23:22:43 +0530

    ui changes

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,16 +1,17 @@
-"use client"
+"use client";
 import Card from '@/components/Card';
 import Loading from "@/components/Loading";
 import { fetchProductsFrom, getProduct, fetchProductsInitial } from '@/lib/features/products/productSlice';
 import { useAppSelector } from '@/lib/hooks';
 import { useAppDispatch } from '@/lib/store';
-import React, {  useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoMdAddCircle } from "react-icons/io";
 import { useDisclosure } from '@chakra-ui/react';
 import AddProductModal from '@/components/AddProductModal';
 import { notFound } from 'next/navigation';
 import { getSubcategories } from '@/lib/features/subcategories';
 import { Product } from '@/lib/types';
+import Tab from './Tab';
 type Category = "pooja" | "cosmetics" | "grocery" | "stationary";
 export default function Products({ category }: { category: Category }) {
   const user = useAppSelector(state => state.authReducer.user);
@@ -20,7 +21,7 @@ export default function Products({ category }: { category: Category }) {
   const loadingConditonA = (sub.length === 0);
 
   const [subcategory, setSubcategory] = useState<string>('all');
-  const subs = useAppSelector(state=>state.subcategoriesReducers[category]);
+  const subs = useAppSelector(state => state.subcategoriesReducers[category]);
 
   useEffect(() => {
     let timeout: any = null;
@@ -44,17 +45,17 @@ export default function Products({ category }: { category: Category }) {
   //   return notFound();
   // }
   const [filtered, setFiltered] = useState<Product[]>([]);
-  useEffect(()=>{
-    const res = sub.filter(s=>{
-      if(subcategory === 'all'){
+  useEffect(() => {
+    const res = sub.filter(s => {
+      if (subcategory === 'all') {
         return true;
       }
-      if(s.category === subcategory){
+      if (s.category === subcategory) {
         return true;
       }
     })
     setFiltered(res);
- 
+
   }, [subcategory, sub])
   return (
     <>
@@ -62,17 +63,7 @@ export default function Products({ category }: { category: Category }) {
         <Loading />
       </div> :
         <>
-        <div className='w-full mx-12'>
-          
-        <select name="subcategory" id="time-range" className='rounded-lg outline-none w-10 min-w-max px-2 py-1' value={subcategory} onChange={e => {
-          setSubcategory(e.target.value)
-        }}>
-          <option value="all">All</option>
-          {
-            subs.map((s, i)=><option key={i} value={s.name}>{s.name}</option>)
-          }
-        </select>
-        </div>
+        <Tab subs={subs} subcategory={subcategory} setSubcategory={setSubcategory}/>
           <div className='grid md:grid-cols-4 place-items-center'>
 
             {
---
+++ src/components/Products/Tab.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useState } from 'react'
+
+export default function Tab({ subs, subcategory, setSubcategory }: { subs: any[], subcategory: string, setSubcategory: any }) {
+    const [activeIndex, setActiveIndex] = useState<number>(-1);
+    console.log(subcategory, activeIndex)
+
+    return (
+        <div
+            className='  my-3 py-3 px-5 flex flex-row gap-1 flex-wrap'
+        >
+            <div
+
+                className={
+                    ` flex justify-center items-center p-2 w-40 h-20 bg-gray-400 text-green-700
+                    hover:bg-green-600 hover:text-white rounded-3xl hover:rounded-xl 
+                    transition-all duration-200 ease-in cursor-pointer
+                    ${activeIndex === -1
+                        ? 'bg-green-600 text-white rounded-xl'
+                        : ' bg-gray-400 text-green-700 rounded-3xl'
+                    }
+                    `
+                }
+                onClick={(e) => {
+                    setSubcategory('all');
+                    setActiveIndex(-1);
+                    
+
+                }}
+            >
+                All
+            </div>
+            {
+                subs.map((sub, i) => (<div
+                    key={i}
+                    className={
+                        ` flex justify-center items-center p-2 w-40 h-20 bg-gray-400 text-green-700
+                    hover:bg-green-600 hover:text-white rounded-3xl hover:rounded-xl 
+                    transition-all duration-200 ease-in cursor-pointer
+                    ${activeIndex !== i
+                            ? ' bg-gray-400 text-green-700 rounded-3xl'
+                            : 'bg-green-600 text-white rounded-xl'
+                        }
+                    `
+                    }
+                    onClick={(e) => {
+                        setSubcategory(sub.name);
+                        setActiveIndex(i);
+
+                    }}
+                >
+                    {sub.name}
+                </div>))
+            }
+
+        </div>
+    )
+}
